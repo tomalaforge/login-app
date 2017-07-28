e797be18730c188241c7b4e57c7d2c79eb168f2d
Thomas Laforge <thomas.laforge@ieva.io>
2017-07-28 09:24:31 +0200

    test keycloak-js

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,21 +1,42 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {ContentPage} from '../content/content';
+import * as Keycloak from 'keycloak-js';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage{
+export class HomePage {
+
+  private keycloak;
+  private auth: any = {};
 
   constructor(public navCtrl: NavController) {
-
+    this.keycloak = Keycloak("assets/keycloak.json");
+    this.keycloak.init()
+      .success(() => {
+        alert(this.keycloak.authenticated?"authenticated":"not authenticated");
+        // this.auth.authenticated = true;
+        // this.auth.authz = this.keycloak;
+        // this.auth.logoutUrl = `${this.keycloak.authServerUrl}/realms/${this.keycloak.realm}/protocol/openid-connect/logout?redirect_uri=/`;
+      })
+      .error(()=>alert("error"));
   }
 
-  pushPage(){
+  login() {
+    this.keycloak.login();
+  }
+
+  logout(){
+    this.auth.loggedIn = false;
+    this.auth.authz = null;
+    window.location.href = this.auth.logoutUrl;
+  }
+
+  pushPage() {
     this.navCtrl.push(ContentPage);
   }
-
 
 
 }
